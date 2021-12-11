f044f429680b51b98b43b69cca717fbaad2513b8
Vitor <>
2021-12-11 03:52:30 -0300

    função formataData

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,3 +2,10 @@ export function formataDinheiro(num) {
   const valor = Number(num).toFixed(2).replace('.', ',');
   return `R$ ${valor}`;
 }
+
+/**
+ * Formata uma string de data ISO em pt-BR
+ */
+export function formataData(data) {
+  return new Date(data).toLocaleDateString('pt-BR');
+}
